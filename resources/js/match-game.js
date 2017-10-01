c9de5ecca4362734508c554da5a271de430d19a2
lcr-8 <solutionslcr@gmail.com>
2017-10-01 10:52:37 -0400

    voie de controunemrent

--- resources/js/match-game.js
+++ resources/js/match-game.js
@@ -68,47 +68,58 @@ MatchGame.renderCards = function(cardValues, $game) {
 MatchGame.flipCard = function($card, $game) {
    //Est-ce que la carte est face contre terre
 console.debug("flip_00");
-   if ($card.data('flipped') == false){
-console.debug("flip_01");
-     //ajout de la classe pour la carte tournée
-     $card.addClass('faceup');
-     //Changer le couleur de fond ==> utilisation de l'attribut "data" de la carte
-     $card.css('background-color', $card.data('color'));
-     //Afficher le nombre
-     $card.text($card.data('value'));
-     //Mettre l'attribut «flipped» à «true»
-     $card.data('flipped', true);
-console.debug("flip_02");
-     $game.data('flippedCards').list.push($card);
-     //S'il s'agit de la 2e carte tournée
-     if ($game.data('flippedCards').list.length == 2) {
-       console.debug("Hourra! 2 cartes");
-       //Valider s'il y a correspondance entre les deux cartes retournées
-       if ($game.data('flippedCards').list[0].data('value') == $game.data('flippedCards').list[1].data('value')){
-         //Si oui, on grise les cartes
-         $game.data('flippedCards').list[0].addClass('matched');
-         $game.data('flippedCards').list[1].addClass('matched');
-console.debug("flip_03_2cartesPareils");
-       } else {
-console.debug("flip_04_reinit");
-         //Si non, on remet les paramètres par défaut de carte et on change l'attribut «flipped» à false
-         //Réinitialisation 1ère carte
-         //setTimeout(function(){
-           console.debug("flip_05_reinit...wait...");
-           $game.data('flippedCards').list[0].removeClass('faceup');
-           $game.data('flippedCards').list[0].css('background-color', 'rgb(32, 64, 86)');
-           $game.data('flippedCards').list[0].empty();
-           $game.data('flippedCards').list[0].data('flipped', false);
-           //Réinitialisation 2e carte
-           $game.data('flippedCards').list[1].removeClass('faceup');
-           $game.data('flippedCards').list[1].css('background-color', 'rgb(32, 64, 86)');
-           $game.data('flippedCards').list[1].empty();
-           $game.data('flippedCards').list[1].data('flipped', false);
-//         }, 578);
-console.debug("flip_06_reinit");
+console.debug("$game.data('flippedCards').list.length ==> " + $game.data('flippedCards').list.length)
+  if ($game.data('flippedCards').list.length < 2) {
+    if ($card.data('flipped') == false){
+ console.debug("flip_01");
+      //ajout de la classe pour la carte tournée
+      $card.addClass('faceup');
+      //Changer le couleur de fond ==> utilisation de l'attribut "data" de la carte
+      $card.css('background-color', $card.data('color'));
+      //Afficher le nombre
+      $card.text($card.data('value'));
+      //Mettre l'attribut «flipped» à «true»
+      $card.data('flipped', true);
+ console.debug("flip_02");
+      $game.data('flippedCards').list.push($card);
+      //S'il s'agit de la 2e carte tournée
+      if ($game.data('flippedCards').list.length == 2) {
+        console.debug("Hourra! 2 cartes");
+        //Valider s'il y a correspondance entre les deux cartes retournées
+        if ($game.data('flippedCards').list[0].data('value') == $game.data('flippedCards').list[1].data('value')){
+          //Si oui, on grise les cartes
+          $game.data('flippedCards').list[0].addClass('matched');
+          $game.data('flippedCards').list[1].addClass('matched');
+          $game.data('flippedCards').list[0].css('background-color', 'rgb(153, 153, 153)');
+          $game.data('flippedCards').list[1].css('background-color', 'rgb(153, 153, 153)');
 
-       }
-       $game.data('flippedCards').list = [];
-     }
-   }
+ console.debug("flip_03_2cartesPareils");
+        }
+      }
+    }
+  } else {
+console.debug("flip_04_reinit");
+    //Si non, on remet les paramètres par défaut de carte et on change l'attribut «flipped» à false
+    //Réinitialisation 1ère carte
+    console.debug("flip_05_reinit...wait...");
+    setTimeout(MatchGame.reInit($card, $game), 1578);
+console.debug("flip_06_reinit");
+    $game.data('flippedCards').list = [];
+    console.debug("reinit liste");
+  }
+};
+
+MatchGame.reInit = function($card, $game){
+  if ($game.data('flippedCards').list[0].hasClass('matched') == false){
+    console.debug("flip_05_1_reinit...POSTwait...");
+    $game.data('flippedCards').list[0].removeClass('faceup');
+    $game.data('flippedCards').list[0].css('background-color', 'rgb(32, 64, 86)');
+    $game.data('flippedCards').list[0].empty();
+    $game.data('flippedCards').list[0].data('flipped', false);
+    //Réinitialisation 2e carte
+    $game.data('flippedCards').list[1].removeClass('faceup');
+    $game.data('flippedCards').list[1].css('background-color', 'rgb(32, 64, 86)');
+    $game.data('flippedCards').list[1].empty();
+    $game.data('flippedCards').list[1].data('flipped', false);
+  }
 };
